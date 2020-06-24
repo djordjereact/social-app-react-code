acfe7894b24e759f1605af1efea61ebf1681d695
djordjereact <djole07mihailovic@gmail.com>
2020-06-24 20:02:33 +0200

    Not really finished delete a scream

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -5,6 +5,7 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import PropTypes from 'prop-types';
 import MyButton from '../util/MyButton';
+import DeleteScream from './DeleteScream';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -17,6 +18,7 @@ import { likeScream, unlikeScream } from '../redux/actions/dataActions';
 
 const styles = {
     card: {
+        position: 'relative',
         display: 'flex',
         marginBottom: 20
     },
@@ -56,11 +58,14 @@ class Scream extends Component {
                 createdAt,
                 userImage,
                 userHandle,
+                screamId,
                 likeCount,
                 commentCount },
                 user: {
-                authenticated
-            } } = this.props;
+                    authenticated,
+                    credentials: { handle }
+                }
+             } = this.props;
         const likeButton = !authenticated ? (
             <MyButton tip="Like">
                 <Link to="/login">
@@ -76,6 +81,10 @@ class Scream extends Component {
                 <FavoriteBorder color="primary" />
             </MyButton>
         );
+
+        const deleteButton = authenticated && userHandle === handle ? (
+            <DeleteScream screamId={screamId}/>
+        ) : null;
         return (
             <div>
                 <Card className={classes.card}>
@@ -88,6 +97,7 @@ class Scream extends Component {
                             variant="h5"
                             component={Link}
                             to={`/users/${userHandle}`} color="primary">{userHandle}</Typography>
+                        {deleteButton}
                         <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
                         <Typography variant="body1">{body}</Typography>
                         {likeButton}
